77b4ade6a533f79508f4a4d7c5bf79a42469d1a0
Guy <guymoustapha@gmail.com>
2020-08-11 17:05:55 +0200

    add my_each_with_index with one testing error

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -8,7 +8,16 @@ describe Enumerable do
     end
     it 'returns the elements of the array' do
       checker = array.my_each { |x| x }
-      expect(checker).to eql?(array)
+      expect(checker).to eq(array)
+    end
+  end
+  describe '#my_each_with_index' do
+    it 'returns an enumerator when no block_given' do
+      expect(array.my_each).to be_instance_of(Enumerator)
+    end
+    it 'returns the sum of array ' do
+      sum = 0
+      expect(array.my_each_with_index { |item| sum += item }).to eql(43)
     end
   end
 end
