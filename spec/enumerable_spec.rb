7569b9f79d72c9945380ebe347e08c40dde73d5a
Guy <guymoustapha@gmail.com>
2020-08-12 09:59:06 +0200

    add my select my all and my any

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -2,7 +2,7 @@ require_relative '../bin/main.rb'
 
 describe Enumerable do
   let(:array) { [1, 3, 5, 7, 9, 8, 10] }
-  context '#my_each' do
+  describe '#my_each' do
     it 'returns an enumerator when no block_given' do
       expect(array.my_each).to be_instance_of(Enumerator)
     end
@@ -21,5 +21,22 @@ describe Enumerable do
       expect(sum).to eql(43)
     end
   end
-  describe '#'
+  describe '#my_select' do
+    it 'returns numbers > 5' do
+      expect(array.my_select { |x| x > 5 }).to eql([7, 9, 8, 10])
+    end
+  end
+  describe '#my_all?' do
+    it 'returns true if all items are true' do
+      expect(array.my_all? { |el| el > 0 }).to eq(true)
+    end
+  end
+
+  describe '#my_any?' do
+    it 'returns true if one element is true' do
+      arr = []
+      checker = !arr.push(array.my_any? { |item| item > 5 }).empty?
+      expect(checker).to eql(true)
+    end
+  end
 end
