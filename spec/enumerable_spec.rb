22bd631cbd874f07beacd854316598836258b85d
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-08-12 11:21:33 +0300

    Add my_any?, my_none? and my_count

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -2,6 +2,7 @@ require_relative '../bin/main.rb'
 
 describe Enumerable do
   let(:array) { [1, 3, 5, 7, 9, 8, 10] }
+  let(:array_polly) { [1, 1, 5, 5, 5, 8, 8] }
   describe '#my_each' do
     it 'returns an enumerator when no block_given' do
       expect(array.my_each).to be_instance_of(Enumerator)
@@ -34,9 +35,28 @@ describe Enumerable do
 
   describe '#my_any?' do
     it 'returns true if one element is true' do
-      arr = []
-      checker = !arr.push(array.my_any? { |item| item > 5 }).empty?
+      checker = array.my_any? { |item| item > 5 }
       expect(checker).to eql(true)
+    end
+  end
+
+  describe '#my_none?' do
+    it 'returns true if all elements return false' do
+      checker = array.my_none? { |item| item > 15 }
+      expect(checker).to eql(true)
+    end
+  end
+
+  describe '#my_count' do
+    context 'when there is parameter' do
+      it 'returns the length of the array' do
+        expect(array.my_count).to eql(7)
+      end
+    end
+    context 'when there is one parameter' do
+      it 'returns how many times do the parameter appears' do
+        expect(array_polly.my_count(5)).to eql(3)
+      end
     end
   end
 end
